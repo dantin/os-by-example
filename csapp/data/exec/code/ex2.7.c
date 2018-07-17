e3316f97bae37c22f62bf5407344fef35e3d0d82
dantin <chengjie.ding@gmail.com>
2018-07-17 13:48:36 +0800

    solution of '2.8'.

---
+++ csapp/data/exec/code/ex2.7.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef unsigned char *byte_pointer;
+
+void show_bytes(byte_pointer start, size_t len)
+{
+    size_t i;
+    for (i = 0; i < len; i++) {
+        printf(" %.2x", start[i]);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    const char *s = "abcdef";
+
+    show_bytes((byte_pointer) s, strlen(s));
+
+    return 0;
+}
